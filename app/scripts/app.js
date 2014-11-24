68671c544a93860ee07353100cc40f75395e84c0
wOvalle <willyovalle16@gmail.com>
2014-11-24 15:51:13 -0400

    fixed navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,4 +17,34 @@ angular.module('fmsApp', [
                 templateUrl: 'views/contactus.html'
                 //                controller: 'con'
             })
+            .state('nosotros', {
+                url: '/nosotros',
+                templateUrl: 'views/aboutus.html'
+                //                controller: 'con'
+            })
+            .state('objetivos', {
+                url: '/objetivos',
+                templateUrl: 'views/objectives.html'
+                //                controller: 'con'
+            })
+            .state('programas', {
+                url: '/programas',
+                templateUrl: 'views/programs.html'
+                //                controller: 'con'
+            })
+            .state('galeria', {
+                url: '/galeria',
+                templateUrl: 'views/gallery.html'
+                //                controller: 'con'
+            })
+            .state('noticias', {
+                url: '/noticias',
+                templateUrl: 'views/news.html'
+                //                controller: 'con'
+            })
+            .state('preguntas', {
+                url: '/preguntas',
+                templateUrl: 'views/faq.html'
+                //                controller: 'con'
+            })
     })
